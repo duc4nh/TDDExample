4382dd441ec4626b48ead81cdcd96ef76ff91d9c
duc4nh <im_online1201@yahoo.com>
2016-02-13 19:34:18 +0700

    Step1-MaxMinFailingTest

---
+++ TDDExample3/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDDExample
+{
+    public class Calculator
+    {
+        public static int Calculate(int a, int b, String n)
+        {
+            return 0;
+        }
+    }
+}
---
+++ TDDExampleTest/CalculatorTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TDDExample;
+
+namespace TDDExampleTest
+{
+    [TestClass]
+    public class CalculatorTest
+    {
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Test_Max_Number_Gt_1000()
+        {
+            Calculator.Calculate(1001, 3, "+");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(IndexOutOfRangeException))]
+        public void Test_Min_Number_Lt_N1000()
+        {
+            Calculator.Calculate(-1001, 3, "+");
+        }
+    }
+}
