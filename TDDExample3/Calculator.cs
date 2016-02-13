742b34b98106f6629d659061e74853c77f123560
duc4nh <im_online1201@yahoo.com>
2016-02-13 20:55:32 +0700

    step1-maxminpassingtest

--- TDDExample3/Calculator.cs
+++ TDDExample3/Calculator.cs
@@ -10,6 +10,22 @@ namespace TDDExample
     {
         public static int Calculate(int a, int b, String n)
         {
+            if (a > 1000)
+            {
+                throw new IndexOutOfRangeException("Input khong duoc vuot qua 1000");
+            }
+            if (b > 1000)
+            {
+                throw new IndexOutOfRangeException("Input khong duoc vuot qua 1000");
+            }
+            if (a < -1000)
+            {
+                throw new IndexOutOfRangeException("Input khong duoc nho hon -1000");
+            }
+            if (b < -1000)
+            {
+                throw new IndexOutOfRangeException("Input khong duoc nho hon -1000");
+            }
             return 0;
         }
     }
